fd092ad4cb755cf5a6fa3e5500728fc20a0ff927
sebasp-125 <sp6027193@gmail.com>
2023-12-01 17:29:14 -0600

    Actualizacion 1/12/23

--- login/cuenta__local.js
+++ login/cuenta__local.js
@@ -1,7 +1,26 @@
 const numero_cuenta = document.getElementById('numero_cuenta');
+const nombre_cuenta = document.getElementById('nombre_cuenta');
 
-// el .toFixed es para convertir el valor a un numero entero. 
+// el .toFixed es para convertir el valor a un número entero.
+numero_cuenta.textContent = (Math.random() * 1000000000).toFixed(0);
 
-numero_cuenta.textContent = (Math.random() * 1000000000).toFixed(0); 
+const Validacion = async () => {
+  try {
+    const response = await axios.get('http://localhost:3000/user');
+    // Obtengamos los datos.
+    const UserData = response.data;
+    console.log("Obteniendo Datos....", UserData);
+    // Mostremos los datos
+    const nombre_cuenta = document.getElementById('nombre_cuenta');
 
+    const nombrEUsuario = UserData.nombre || 'Nombre no disponible';
+    const contrasenaUsuario = UserData.contrasena || 'Contraseña no disponible';
+    const UserInfoHTML = `<p>ID: ${contrasenaUsuario}</p><p>Nombre: ${nombrEUsuario}</p>`;
+    
+    nombre_cuenta.innerHTML = UserInfoHTML;
+  } catch (error) {
+    console.error('Error al obtener los datos del usuario:', error);
+  }
+};
 
+Validacion();
